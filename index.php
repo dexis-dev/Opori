3faea3122b556a7a660d9b0bcc805f5361ca6111
Dexis <dexisdeveloper@gmail.com>
2026-07-19 22:35:31 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        margin: 0px;
+        font-family: sans-serif;
+        background-image: url(https://kartinki.pics/uploads/posts/2021-04/1617302238_17-p-svetlii-fon-dlya-saita-19.png);
+    }
+    .container {
+        text-align: center;
+        display: block;
+        max-width: 500px;
+        margin: auto;
+    }
+    button {
+        width: 50%;
+        margin-top: 10px;
+        height: 50px;
+        border-radius: 20px;
+        cursor: pointer;
+        border: 1px solid rgb(7, 7, 43);
+        background-color: #fff;
+        transition: transform 0.3s linear;
+        font-size: 19px;
+       
+    }
+    button:hover {
+        transform: scaleX(1.1);
+        color: #fff;
+        background-color: rgb(7, 7, 43);
+    }
+</style>
+<body>
+    <div class="container">
+        <h1>
+            Это переходник
+        </h1>
+        <a href="OTF/index.php">
+            <button>
+                OTF
+            </button>
+        </a>
+        <a href="OTP/index.php">
+            <button>
+                OTP
+            </button>
+        </a>
+        <a href="SF/index.php">
+            <button>
+                SF
+            </button>
+        </a>
+        <a href="SF — 2/index.php">
+            <button>
+                SF - 2
+            </button>
+        </a>
+        <a href="SP/index.php">
+            <button>
+                SP
+            </button>
+        </a>
+        <a href="SP — 2/index.php">
+            <button>
+                SP - 2
+            </button>
+        </a>
+        <a href="ZDF/index.php">
+            <button>
+                ZDF
+            </button>
+        </a>
+        <a href="ZDF — 2/index.php">
+            <button>
+                ZDF - 2
+            </button>
+        </a>
+    </div>
+</body>
+</html>
